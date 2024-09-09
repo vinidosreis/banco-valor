774295ff0d653f36d7113a2cd0252eef03c00a37
Vinícius R <vinidosreis13@gmail.com>
2024-09-09 04:38:49 -0300

    Initial Commit

---
+++ BancoValor/BancoValorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BancoValorApp.swift
+//  BancoValor
+//
+//  Created by Vinícius dos Reis on 09/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BancoValorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BancoValor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BancoValor
+//
+//  Created by Vinícius dos Reis on 09/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
